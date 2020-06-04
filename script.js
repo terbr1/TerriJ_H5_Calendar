013a5764d8b475dfbdcb63c41021514b1af0ddc6
Terri J <terbr@hotmail.com>
2020-06-03 22:32:27 -0700

    added save button

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //displaying current day
 $("#currentDay").text(moment().format("MMM Do YY"));
+
+//save button
+$(".saveBtn").on("click", funtion(),{
+
+    var: message = $(this).siblings(".description").val(),
+    var: time = $(this).parent(".description").val(),
+    
+    localStorage,setItem(time, message)
+    
+});
+
+
